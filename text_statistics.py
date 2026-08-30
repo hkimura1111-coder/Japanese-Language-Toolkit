b7b7fb35067b6e746e4c7a252252745be2e77d34
H Power <hkimura1111@gmail.com>
2026-08-30 22:01:23 +1200

    Added most common character analysis

--- text_statistics.py
+++ text_statistics.py
@@ -6,7 +6,10 @@ for character in text:
     if character not in counts:
         counts[character] = 1
     else:
-        counts[character] = counts[character] + 1
+        counts[character] = counts[character] + 1 
+        
+highest = 0
+most_common = ""
         
 print("Total characters:", len(text))
 print("Unique characters:", len(counts))
@@ -16,3 +19,14 @@ print("Character frequencies:")
 
 for character in counts:
     print(character, ":", counts[character])
+    
+print()    
+
+for character in counts:
+     if counts[character] > highest:
+         highest = counts[character]
+         most_common = character
+
+print()
+print("Most common character:", most_common)
+print("Frequency:", highest)
